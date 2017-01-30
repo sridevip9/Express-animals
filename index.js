973bbb7c6333bdff251eb78daf3d4c8ffec7089e
sridevi peddi <sridevip9@yahoo.com>
2017-01-30 10:15:32 -0600

    new commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var animals = require('./animals')
+var app = express();
+
+
+app.use(bodyParser.json());
+app.use('/', animals);
+
+app.listen(3000, function() {
+    console.log('server listening port 3000....')
+});
